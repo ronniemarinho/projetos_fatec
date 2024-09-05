5bb3c680a2b4a3c69c2e93a9d6d225466b70da90
Ronnie Shida Marinho <113137267+ronniemarinho@users.noreply.github.com>
2024-09-05 14:03:37 -0300

    Update naive_stream.py

--- naive_stream.py
+++ naive_stream.py
@@ -22,7 +22,7 @@ st.title('Classificação de Risco de Crédito')
 col1, col2, col3 = st.columns([1, 2, 1])  # Definindo uma estrutura de colunas com proporções 1:2:1
 
 with col2:
-    st.image('/Users/ronnieshida/PycharmProjects/novoprojeto/img.png', width=350)
+    st.image('img.png', width=350)
 #
 st.header('Insira os dados do cliente:')
 historia = st.selectbox('História de crédito', ['Boa (0)', 'Desconhecida (1)', 'Ruim (2)'])
